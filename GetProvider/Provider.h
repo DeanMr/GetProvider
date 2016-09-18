d894a9c9710b4133d4081e3683263b6db8eb0d66
UBDean <deanlzwei@gmail.com>
2016-09-18 12:22:27 +0800

    获取当前手机的运营商信息

---
+++ GetProvider/Provider.h
@@ -0,0 +1,13 @@
+//
+//  Provider.h
+//  GetProvider
+//
+//  Created by 云宝 Dean on 16/9/18.
+//  Copyright © 2016年 云宝 Dean. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Provider : NSObject
+
+@end
---
+++ GetProvider/Provider.m
@@ -0,0 +1,13 @@
+//
+//  Provider.m
+//  GetProvider
+//
+//  Created by 云宝 Dean on 16/9/18.
+//  Copyright © 2016年 云宝 Dean. All rights reserved.
+//
+
+#import "Provider.h"
+
+@implementation Provider
+
+@end
