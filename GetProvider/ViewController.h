59bf08a2b6e3fabf35ba32350ab2324ac0141e16
UBDean <deanlzwei@gmail.com>
2016-09-18 11:50:08 +0800

    Initial Commit

---
+++ GetProvider/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GetProvider
+//
+//  Created by 云宝 Dean on 16/9/18.
+//  Copyright © 2016年 云宝 Dean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
